0670d3ff71790f4c8900c0652253dad26b15f0e7
Kiệt Nguyễn Hoàng <20520068@ms.uit.edu.vn>
2021-09-13 21:34:17 +0700

    update beep

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -7,7 +7,12 @@ namespace Test
         static void Main(string[] args)
         {
             while (true)
+            {
                 Console.Beep(500, 500);
+                Console.Beep(1000, 500);
+                Console.Beep(500, 500);
+                Console.Beep(300, 500);
+            }
         }
     }
 }
