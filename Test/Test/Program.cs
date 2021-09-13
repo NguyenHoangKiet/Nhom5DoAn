a74a5b922dfc6cb3fc92e86873e5ec12f0e60ada
Kiệt Nguyễn Hoàng <20520068@ms.uit.edu.vn>
2021-09-13 21:03:50 +0700

    add c# file

---
+++ Test/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+                Console.Beep(500, 500);
+        }
+    }
+}
