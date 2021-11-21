b88458152aa38d210be60279ea94794f2d317a68
Hong Truong Vinh <hongvinhkrn@gmail.com>
2021-11-21 20:36:37 +0700

    Them controlbar vao WelcomeWindow

--- FamilyTree/UserControlForFamilyTree/ControlBarUC.xaml.cs
+++ FamilyTree/UserControlForFamilyTree/ControlBarUC.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FamilyTree.ViewModel;
 
 namespace FamilyTree.UserControlForFamilyTree
 {
@@ -20,9 +21,12 @@ namespace FamilyTree.UserControlForFamilyTree
     /// </summary>
     public partial class ControlBarUC : UserControl
     {
+        public ControlBarViewModel ViewModel { get; set; }
+
         public ControlBarUC()
         {
             InitializeComponent();
+            this.DataContext = ViewModel = new ControlBarViewModel();
         }
     }
 }
