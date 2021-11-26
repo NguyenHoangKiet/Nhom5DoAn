2b836a910dc2be26731a139261f43e5ee9c007fc
Quân Lê Minh <20520708@ms.uit.edu.vn>
2021-11-26 10:42:38 +0700

    Add class OPCUtillity and class Story

---
+++ FamilyTreeLibrary/OPCUtillity.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.IO;
+using System.IO.Packaging;
+using System.Net.Mime;
+
+namespace FamilyTreeLibrary
+{
+    public class OPCUtility
+    {
+        private const string PackageRelationshipType = @"";
+        private const string ResourceRelationshipType = @"";
+
+        #region Write Package
+        public static void CreatePackage(string PackageFileName, string TargetDirectory)
+        {
+            using (Package package = Package.Open(PackageFileName, FileMode.Create))
+            {
+                DirectoryInfo mainDirectory = new DirectoryInfo(TargetDirectory);
+                CreatePart(package, mainDirectory, false);
+                foreach (DirectoryInfo di in mainDirectory.GetDirectories())
+                {
+                    CreatePart(package, di, true);
+                }
+            }
+        }
+
+        private static void CreatePart(Package package, DirectoryInfo directoryInfo, bool storeInDirectory)
+        {
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                switch (file.Extension.ToLower())
+                {
+                    case ".xml":
+                        CreateDocumentPart(package, file, MediaTypeNames.Text.Xml, storeInDirectory);
+                        break;
+                    case ".jpg":
+                        CreateDocumentPart(package, file, MediaTypeNames.Image.Jpeg, storeInDirectory);
+                        break;
+                    case ".gif":
+                        CreateDocumentPart(package, file, MediaTypeNames.Image.Gif, storeInDirectory);
+                        break;
+                    case ".rtf":
+                        CreateDocumentPart(package, file, MediaTypeNames.Text.RichText, storeInDirectory);
+                        break;
+                    case ".txt":
+                        CreateDocumentPart(package, file, MediaTypeNames.Text.Plain, storeInDirectory);
+                        break;
+                    case ".html":
+                        CreateDocumentPart(package, file, MediaTypeNames.Text.Html, storeInDirectory);
+                        break;
+                }
+            }
+        }
+        private static void CreateDocumentPart(Package package, FileInfo file, string contentType, bool storeInDirectory)
+        {
+            Uri partUriDocument;
+            if (storeInDirectory)
+            {
+                partUriDocument =
+                  PackUriHelper.CreatePartUri(new Uri(Path.Combine(file.Directory.Name, file.Name), UriKind.Relative));
+            }
+            else
+            {
+                partUriDocument = PackUriHelper.CreatePartUri(new Uri(file.Name, UriKind.Relative));
+            }
+            PackagePart packagePartDocument = package.CreatePart(partUriDocument, contentType);
+            using (FileStream fileStream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+            {
+                CopyStream(fileStream, packagePartDocument.GetStream());
+            }
+            package.CreateRelationship(packagePartDocument.Uri, TargetMode.Internal, PackageRelationshipType);
+        }
+
+        #endregion
+
+        #region ReadPackage
+        public static void ExtractPackage(string packagePath, string targetDirectory)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(targetDirectory);
+                if (directoryInfo.Exists)
+                {
+                    directoryInfo.Delete(true);
+                }
+
+                directoryInfo.Create();
+            }
+            catch
+            {
+            }
+            using (Package package = Package.Open(packagePath, FileMode.Open, FileAccess.Read))
+            {
+                PackagePart documentPart = null;
+                PackagePart resourcePart = null;
+                Uri uriDocumentTarget = null;
+                foreach (PackageRelationship relationship in package.GetRelationshipsByType(PackageRelationshipType))
+                {
+                    uriDocumentTarget = PackUriHelper.ResolvePartUri(new Uri("/", UriKind.Relative), relationship.TargetUri);
+                    documentPart = package.GetPart(uriDocumentTarget);
+                    ExtractPart(documentPart, targetDirectory);
+                }
+                Uri uriResourceTarget = null;
+                foreach (PackageRelationship relationship in documentPart.GetRelationshipsByType(ResourceRelationshipType))
+                {
+                    uriResourceTarget = PackUriHelper.ResolvePartUri(documentPart.Uri, relationship.TargetUri);
+                    resourcePart = package.GetPart(uriResourceTarget);
+                    ExtractPart(resourcePart, targetDirectory);
+                }
+            }
+        }
+        private static void ExtractPart(PackagePart packagePart, string targetDirectory)
+        {
+            string pathToTarget = targetDirectory;
+            string stringPart = packagePart.Uri.ToString().TrimStart('/');
+            Uri partUri = new Uri(stringPart, UriKind.Relative);
+            Uri uriFullPartPath =
+             new Uri(new Uri(pathToTarget, UriKind.Absolute), partUri);
+            Directory.CreateDirectory(Path.GetDirectoryName(uriFullPartPath.LocalPath));
+            using (FileStream fileStream = new FileStream(uriFullPartPath.LocalPath, FileMode.Create))
+            {
+                CopyStream(packagePart.GetStream(), fileStream);
+            }
+        }
+
+        #endregion
+        private static void CopyStream(Stream source, Stream target)
+        {
+            const int bufSize = 0x1000;
+            byte[] buf = new byte[bufSize];
+            int bytesRead = 0;
+
+            while ((bytesRead = source.Read(buf, 0, bufSize)) > 0)
+            {
+                target.Write(buf, 0, bytesRead);
+            }
+        }
+    }
+}
---
+++ FamilyTreeLibrary/Story.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Xml.Serialization;
+
+namespace FamilyTreeLibrary
+{
+    [Serializable]
+    public class Story : INotifyPropertyChanged
+    {
+        #region Fields and Constants
+        public static class Const
+        {
+            public const string StoriesFolderName = "Stories";
+        }
+
+        private string relativePath;
+
+        #endregion
+
+        #region Properties
+        public string RelativePath
+        {
+            get { return relativePath; }
+            set
+            {
+                if (relativePath != value)
+                {
+                    relativePath = value;
+                    OnPropertyChanged(nameof(relativePath));
+                }
+            }
+        }
+        [XmlIgnore]
+        public string AbsolutePath
+        {
+            get
+            {
+                string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                    App.FolderName);
+                tempFolder = Path.Combine(tempFolder, App.SubFolderName);
+
+                if (relativePath != null)
+                {
+                    return Path.Combine(tempFolder, relativePath);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Story() { }
+
+        #endregion
+
+        #region Methods
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "Microsoft.FamilyShowLib.Story.Save(System.String,System.String):System.Void")]
+        public void Save(TextRange storyText, string storyFileName)
+        {
+            string dataFormat = DataFormats.Rtf;
+
+            string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                App.FolderName);
+            tempFolder = Path.Combine(tempFolder, App.SubFolderName);
+            string storiesLocation = Path.Combine(tempFolder, Const.StoriesFolderName);
+            storyFileName = GetSafeFileName(storyFileName);
+            string storyAbsolutePath = Path.Combine(storiesLocation, storyFileName);
+
+            try
+            {
+                if (!Directory.Exists(storiesLocation))
+                    Directory.CreateDirectory(storiesLocation);
+                using (FileStream stream = File.Create(storyAbsolutePath))
+                {
+                    relativePath = Path.Combine(Const.StoriesFolderName, storyFileName);
+                    if (storyText.CanSave(dataFormat))
+                    {
+                        storyText.Save(stream, dataFormat);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public void Load(TextRange storyText)
+        {
+            string dataFormat = DataFormats.Rtf;
+
+            if (File.Exists(AbsolutePath))
+            {
+                try
+                {
+                    using (FileStream stream = File.OpenRead(AbsolutePath))
+                    {
+                        if (storyText.CanLoad(dataFormat))
+                        {
+                            storyText.Load(stream, dataFormat);
+                        }
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Scope = "member", Target = "Microsoft.FamilyShowLib.Story.Save(System.String,System.String):System.Void")]
+        public void Save(string storyText, string storyFileName)
+        {
+            string dataFormat = DataFormats.Rtf;
+
+            string tempFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                App.FolderName);
+            tempFolder = Path.Combine(tempFolder, App.SubFolderName);
+            string storiesLocation = Path.Combine(tempFolder, Const.StoriesFolderName);
+            storyFileName = GetSafeFileName(storyFileName);
+            string storyAbsolutePath = Path.Combine(storiesLocation, storyFileName);
+            TextBlock block = new TextBlock();
+            block.Text = storyText;
+            TextRange range = new TextRange(block.ContentStart, block.ContentEnd);
+
+            try
+            {
+                if (!Directory.Exists(storiesLocation))
+                {
+                    Directory.CreateDirectory(storiesLocation);
+                }
+                using (FileStream stream = File.Create(storyAbsolutePath))
+                {
+                    relativePath = Path.Combine(Const.StoriesFolderName, storyFileName);
+                    if (range.CanSave(dataFormat))
+                    {
+                        range.Save(stream, dataFormat);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+        public void Delete()
+        {
+            if (File.Exists(AbsolutePath))
+            {
+                try
+                {
+                    File.Delete(AbsolutePath);
+                }
+                catch
+                {
+                }
+            }
+        }
+        private static string GetSafeFileName(string fileName)
+        {
+            char[] invalid = Path.GetInvalidFileNameChars();
+            int pos;
+            while ((pos = fileName.IndexOfAny(invalid)) != -1)
+            {
+                fileName = fileName.Remove(pos, 1);
+            }
+
+            return fileName;
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+    }
+}
