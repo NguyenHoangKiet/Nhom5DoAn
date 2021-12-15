e6059f7cbf86818953a7b7be6d520ac48ca255fd
Quân Lê Minh <20520708@ms.uit.edu.vn>
2021-12-16 02:03:21 +0700

    Update Filter For List And Update New User UI

--- FamilyTree/Components/FamilyData.xaml.cs
+++ FamilyTree/Components/FamilyData.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FamilyTreeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,37 @@ namespace FamilyTree.Components
     /// </summary>
     public partial class FamilyData : Window
     {
+        public PeopleCollection peopleCollection;
         public FamilyData()
         {
             InitializeComponent();
+        }
+        public FamilyData(PeopleCollection people)
+        {
+            InitializeComponent();
+            peopleCollection = people;
+            listviewMember.ItemsSource = peopleCollection;
+
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(listviewMember.ItemsSource);
+            view.Filter = UserFilter;
+        }
+
+        private bool UserFilter(object obj)
+        {
+            if (String.IsNullOrEmpty(txtFilter.Text))
+                return true;
+            else
+                return ((obj as Person).FullName.IndexOf(txtFilter.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void PackIcon_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(listviewMember.ItemsSource).Refresh();
         }
     }
 }
